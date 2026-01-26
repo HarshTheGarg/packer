526dbe590989a784fda7a737e88c19cfc9b263c1
Harsh Garg <harshthegarg@gmail.com>
2026-01-26 21:17:43 +0530

    Add index page

---
+++ back/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const mongoUri =
+  process.env.MONGO_URI || "mongodb://localhost:27017/mydatabase";
+
+const connectToDatabase = async () => {
+  await mongoose.connect(mongoUri);
+};
+
+// Connect to the database
+connectToDatabase()
+  .then(() => {
+    console.log(`Connected to MongoDB at ${mongoUri}`);
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+  });
+
+// Enable CORS for all routes
+app.use(cors({ origin: "http://localhost", credentials: true }));
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+
+const testRoutes = require("./src/routes/test.routes");
+app.use("/api/v1.0/tests", testRoutes);
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
