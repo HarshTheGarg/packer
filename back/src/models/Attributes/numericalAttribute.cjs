2b426deb820298ccd6b150b19fce71a911ddd183
Harsh Garg <harshthegarg@gmail.com>
2026-01-27 22:48:40 +0530

    Add 'unit' in schema

--- back/src/models/Attributes/numericalAttribute.cjs
+++ back/src/models/Attributes/numericalAttribute.cjs
@@ -2,24 +2,25 @@ const mongoose = require("mongoose");
 const Attribute = require("./attribute");
 
 const numericalAttributeSchema = new mongoose.Schema({
-  min: { type: Number, required: false },
-  max: { type: Number, required: false },
+    min: { type: Number, required: false },
+    max: { type: Number, required: false },
+    unit: { type: String, required: false, default: "Nos" },
 });
 
-numericalAttributeSchema.pre("validate", function (next) {
-  if (this.min !== undefined && this.max !== undefined && this.min > this.max) {
-    return next(
-      new Error(
-        `Minimum value ${this.min} cannot be greater than maximum value ${this.max}`,
-      ),
-    );
-  }
-  next();
+numericalAttributeSchema.pre("validate", function(next) {
+    if (this.min !== undefined && this.max !== undefined && this.min > this.max) {
+        return next(
+            new Error(
+                `Minimum value ${this.min} cannot be greater than maximum value ${this.max}`,
+            ),
+        );
+    }
+    next();
 });
 
 const NumericalAttribute = Attribute.discriminator(
-  "NumericalAttribute",
-  numericalAttributeSchema,
+    "NumericalAttribute",
+    numericalAttributeSchema,
 );
 
 module.exports = NumericalAttribute;
